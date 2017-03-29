e440f5b094cf209350bb7fa7f0e693ed5b462765
Jessica Thrasher <jthrashe@cisco.com>
2017-03-28 19:13:34 -0700

    Adding simple app

--- pollsApp/pollsReact/polls/src/App.js
+++ pollsApp/pollsReact/polls/src/App.js
@@ -1,21 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Request from 'react-http-request';
+import { Button } from 'reactstrap';
 
-class App extends Component {
+// Displays available polls for voting
+export default class App extends Component {
+
+  // votes on a poll
+  vote(option, id) {
+    var url = 'http://localhost:8090/polls/vote/' + id + '/' + option;
+
+    fetch(url, {
+      method: 'POST',
+      headers: {}
+    })
+  }
+
   render() {
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Request
+        url='http://localhost:8090/polls/list'
+        method='get'
+        accept='application/json'
+        verbose={true}
+      >
+        {
+          ({error, result, loading}) => {
+            if (loading) {
+              return <div>loading...</div>;
+            } else {
+              var fullResponse = JSON.parse(result.text);
+              var polls = fullResponse.polls;
+
+              return (
+                <div>
+                  {polls.map(function(object, i){
+                    return <div>
+                        <div>
+                          {object.title}
+                        </div>
+                        <div>
+                          <Button color="primary" onClick={() => this.vote(1, object.id)}>{object.option1}</Button>{' '}
+                          <Button color="primary" onClick={() => this.vote(2, object.id)}>{object.option2}</Button>
+                        </div>
+                        <div>
+                          Current results: {object.option1}: {object.votes1} {object.option2}: {object.votes2}
+                        </div>
+                        <hr></hr>
+                      </div>
+                  }, this)}
+                </div>
+              );
+            }
+          }
+        }
+      </Request>
     );
   }
 }
-
-export default App;
